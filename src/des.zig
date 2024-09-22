930cd42db692fd7f2bd2ae432045bddf55501868
jwstolk <jwstolk@gmail.com>
2024-09-22 02:00:54 +0200

    more DES cleanups

--- src/des.zig
+++ src/des.zig
@@ -17,82 +17,62 @@ pub fn init_decrypt(key: u64, subkeys: *[16]u48) void {
 pub fn process_8bytes(block: *[8]u8, subkeys: *const [16]u48) void {
     const block_as_int: u64 = std.mem.readInt(u64, block, .big);
     var out: u64 = 0;
-    for (0..64) |i| {
-        const col = i & 0x7;
-        const row = i >> 3;
-        const IP_from_64:u6 = @intCast(IP_first_col[row] + (col * 8));  // = 63 - (IP - 1)
-        out |= @intCast(((@as(u64, @as(u64, 1) << IP_from_64) & block_as_int) >> IP_from_64) << @intCast(63 - i));
+    for (0..8) |row| {
+        for (0..8) |col| {
+            const IP_from_64: u6 = @intCast(IP_first_col[row] + (col * 8));  // = 63 - (IP - 1)
+            out = (out << 1) | ((block_as_int >> IP_from_64) & 0x1);
+        }
     }
-    var left_half:  u32 = @truncate((out >> 32));
-    var right_half: u32 = @truncate( out );
-    for (0..16) |i| {
-        const prev_left_half = left_half;
-        left_half  = right_half;
-        right_half = prev_left_half ^ applyFeistelRound(right_half, subkeys[i]);
-    }
-
-    const after_16_rounds: u64 = (@as(u64, right_half) << 32) | @as(u64, left_half);
-    out = 0;
-    for (0..64) |i| {
-        const col = i & 0x7;
-        const row = i >> 3;
-        const IP_1_from_64:u6 = @intCast(@as(u6, IP_1_first_row[col]) * 8 + row);  // = 63 - (IP_1 - 1)        
-        out |= (((@as(u64, 1) << IP_1_from_64) & after_16_rounds) >> IP_1_from_64) << @intCast(63 - i);
+    for (0..16) |i| out = (out << 32) | ((out >> 32) ^ applyFeistelRound(@truncate(out), subkeys[i]));
+    const after_16_rounds = out;  // the final swap of left/right 32 bit is handled by the IP_1 table.
+    for (0..8) |row| {
+        for (0..8) |col| {
+            const IP_1_from_64:u6 = @intCast(@as(u6, IP_1_first_row[col]) * 8 + row);  // = 63 - (IP_1 - 1)        
+            out = (out << 1) | ((after_16_rounds >> IP_1_from_64) & 0x1); // old value will be shifted out.
+        }
     }
     std.mem.writeInt(u64, block, out, .big);
 }
 
 fn generateKeys(key: u64, subkeys: *[16]u48) void {
     var stripped_key: u56 = 0;
-    for (0..56) |i| {
-        const col_4   = (i >> 2) & 0x1;
-        const row     =  i >> 3;
-        const col_ofs =  i & 0x3;
-        const PC1_from_64:u6 = @intCast(PC1_col_0and4[row][col_4] + (col_ofs * 8));  // = 63 - (PC1 - 1)        
-        stripped_key |= @intCast(((@as(u64, @as(u64, 1) << PC1_from_64) & key) >> PC1_from_64) << @intCast(55 - i));
+    for (0..7) |row| {
+        for (0..8) |col| {
+            const PC1_from_64:u6 = @intCast(PC1_col_0and4[row][col >> 2] + ((col & 0b11) * 8));  // = 63 - (PC1 - 1)        
+            stripped_key = (stripped_key << 1) | @as(u1, @intCast((key >> PC1_from_64) & 0x1));
+        }
     }
-    var key_lhs: u28 = @intCast((stripped_key & @as(u56, 0xF_FF_FF_FF) << 28) >> 28);
-    var key_rhs: u28 = @intCast( stripped_key & @as(u56, 0xF_FF_FF_FF)             );
     for (0..16) |i| { 
         const extra_rotation: usize = (@as(u16, 0b01111110_11111100) >> @intCast(i)) & 0x1;
-        for (0..1+extra_rotation) |_| { 
-            key_lhs = (key_lhs << 1) | (key_lhs & @as(u28, 1 << 27)) >> 27;
-            key_rhs = (key_rhs << 1) | (key_rhs & @as(u28, 1 << 27)) >> 27;
-        }
-        const subkey: u56 = (@as(u56, @intCast(key_lhs)) << 28) | @as(u56, @intCast(key_rhs));
-        var permutated_subkey: u48 = 0;    
-        for (&PC2_FROM_56, 0..48) |pc2_from_56, j| {
-            permutated_subkey |= @intCast((((@as(u56, 1) << pc2_from_56) & subkey) >> pc2_from_56) << @intCast(47 - j));
-        }
-        subkeys.*[i] = permutated_subkey;
+        for (0..1+extra_rotation) |_| stripped_key = ((stripped_key & 0x7FFFFFF_7FFFFFF) <<  1) |
+                                                     ((stripped_key & 0x8000000_8000000) >> 27);
+        var subkey: u48 = 0;    
+        for (0..48) |j| subkey = (subkey << 1) | @as(u1, @intCast((stripped_key >> PC2_FROM_56[j]) & 0x1));
+        subkeys.*[i] = subkey;
     }
 }
 
 fn applyFeistelRound(right_half: u32, subkey: u48) u32 {
     var expanded_right_half: u48 = 0;
-    for (0..48) |i| {
-        const row      = @divFloor(i, 6);        // every 6 indexes, there is an extra -2 shift, mapping the 6*8=48 inputs to 32 outputs.
-        const unbound  = i - (row * 2);          //  0, 1..32, 33
-        const bound    = @mod(unbound + 31, 32); // 31, 0..31, 0 
-        const shift:u5 = @intCast(31 - bound);   //  0, 31..0, 31 
-        expanded_right_half |= (@as(u48, ((@as(u32, 1) << shift) & right_half) >> shift) << @intCast(47 - i));
+    for (0..48) |i| { // Expansion function:
+        const row     = @divFloor(i, 6);
+        const unbound = i - (row * 2);          //  0, 1..32, 33
+        const bound   = @mod(unbound + 31, 32); // 31, 0..31, 0 
+        const E:u5    = @intCast(31 - bound);   //  0, 31..0, 31 
+        expanded_right_half = (expanded_right_half << 1) | ((right_half >> E) & 0x1);
     }
     const keyed_expansion: u48 = expanded_right_half ^ subkey;
-
     var sbox_output: u32 = 0;
     for (0..8) |s_box_idx| {
         const six_bit_chunk: u6 = @truncate(keyed_expansion >> @intCast(6 * ((S_TABLES.len - 1) - s_box_idx)));
-        const row:   usize = (six_bit_chunk & 0x1) | ((six_bit_chunk & 0x20) >> 4); // bit 0 and 5
-        const col:   usize = (six_bit_chunk >> 1) & 0xF;                            // bit  1...4
+        const row:   usize = (six_bit_chunk & 0b1) | ((six_bit_chunk & 0b10_0000) >> 4); // bit 0 and 5
+        const col:   usize = (six_bit_chunk >> 1) & 0b1111;                              // bit  1...4
         const s_box: u32 = S_TABLES[s_box_idx][row][col];
         sbox_output |= s_box << @as(u5, @intCast(4 * ((S_TABLES.len - 1) - s_box_idx)));
     }
-
-    var permuted_sbox_output: u32 = 0; 
-    for (&P_FROM_32, 0..32) |p_from_32, j| {
-        permuted_sbox_output |= (((@as(u32, 1) << p_from_32) & sbox_output) >> p_from_32) << @intCast(31 - j);
-    }
-    return permuted_sbox_output;
+    var permuted: u32 = 0; 
+    for (0..32) |i| permuted = (permuted << 1) | ((sbox_output >> P_FROM_32[i]) & 0x1);
+    return permuted;
 }
 
 // Key Permuted Choice 1:
@@ -121,8 +101,8 @@ const PC2_FROM_56 = [_]u6{  // = 55 - (PC2 - 1)
 // Initial Permutation:
 const IP_first_col = [8]u3{ 6, 4, 2, 0, 7, 5, 3, 1};  // = 63 - (IP - 1)
 
-// Inverse Initial Permutation:
-const IP_1_first_row = [8]u3{ 3, 7, 2, 6, 1, 5, 0, 4};  // = (63 - (IP_1 - 1)) / 8
+// Inverse Initial Permutation:  (now includes the final left/right 32bit swap)
+const IP_1_first_row = [8]u3{ 7, 3, 6, 2, 5, 1, 4, 0};  // = (63 - (IP_1 - 1)) / 8
 
 // Permutation:
 const P_FROM_32 = [_]u5 {  // = 31 - (P - 1)
@@ -208,9 +188,8 @@ test "DES Test Vectors" {
         const key             = test_vector[0];
         const plaintext       = test_vector[1];
         const expected_cipher = test_vector[2];
-
-        var block:    [8]u8  = [_]u8{0} ** 8;
-        var subkeys: [16]u48 = .{0} ** 16;
+        var block:     [8]u8  = [_]u8{0} ** 8;
+        var subkeys:  [16]u48 = .{0} ** 16;
 
         // test encryption:
         std.mem.writeInt(u64, &block, plaintext, .big);
