fe5dab78f87c55b5f978d3da4bc81763a9508fc8
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2025-09-13 22:02:18 +0200

    Update to 0.15.1; Add fingerprint

--- build.zig
+++ build.zig
@@ -12,24 +12,36 @@ pub fn build(b: *std.Build) void {
     });
     vnc_mod.addImport("network", network_mod);
 
-    const client_exe = b.addExecutable(.{
-        .name = "zvnc-client",
+    const client_mod = b.createModule(.{
         .root_source_file = b.path("src/client-main.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "network", .module = network_mod },
+            .{ .name = "vnc", .module = vnc_mod },
+        },
     });
-    client_exe.root_module.addImport("network", network_mod);
-    client_exe.root_module.addImport("vnc", vnc_mod);
+
+    const client_exe = b.addExecutable(.{
+        .name = "zvnc-client",
+        .root_module = client_mod,
+    });
     b.installArtifact(client_exe);
 
-    const server_exe = b.addExecutable(.{
-        .name = "zvnc-server",
+    const server_mod = b.createModule(.{
         .root_source_file = b.path("src/server-main.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "network", .module = network_mod },
+            .{ .name = "vnc", .module = vnc_mod },
+        },
     });
-    server_exe.root_module.addImport("network", network_mod);
-    server_exe.root_module.addImport("vnc", vnc_mod);
+
+    const server_exe = b.addExecutable(.{
+        .name = "zvnc-server",
+        .root_module = server_mod,
+    });
     b.installArtifact(server_exe);
 
     const run_client_cmd = b.addRunArtifact(client_exe);
@@ -45,10 +57,14 @@ pub fn build(b: *std.Build) void {
     const run_client_step = b.step("run-client", "Run the app");
     run_client_step.dependOn(&run_client_cmd.step);
 
-    const exe_tests = b.addTest(.{
+    const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/vnc.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe_mod,
     });
 
     const test_step = b.step("test", "Run unit tests");
